af9be8f1c0966cc40be4373289278f2c53c7cd03
hoho3028 <144401653+hoho3028@users.noreply.github.com>
2023-09-08 14:06:36 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include  'head.php'; ?>
+	<body style="padding:0;margin:0;" id="index">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="js/Backstretch.js" type="text/javascript"></script>
+		<style></style>
+		<?php include  'header.php'; ?>
+		<!--<div id="back"></div>--->
+	</body>
+	<script>
+		$.backstretch(["./image/1.png","./image/2.png","./image/3.png"],{duration:3000,fade:1000});
+	</script>
+<html>
